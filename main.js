5de784bab0b0be2aba070344c02973d127d013d3
Moustafa El Dehni <moustafa.eldehni@hotmail.com>
2025-04-27 02:45:07 +0200

    Add quelques icones pour mobile, responsive pour mobile

--- main.js
+++ main.js
@@ -50,14 +50,20 @@ btnAjouter.addEventListener("click", ()=>{
         divBtnTache.className = "divButtonTache"
         
         let newModif = document.createElement("button")
-        newModif.innerText = "Modifier"
-        
+        newModif.className = "btnsTache"
+        let newModifText = document.createElement("span")
+        newModifText.innerText = "Modifier"
+        let newModifSvg = document.createElement("svg")
+        newModifSvg.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px"><path d="M216-216h51l375-375-51-51-375 375v51Zm-72 72v-153l498-498q11-11 23.84-16 12.83-5 27-5 14.16 0 27.16 5t24 16l51 51q11 11 16 24t5 26.54q0 14.45-5.02 27.54T795-642L297-144H144Zm600-549-51-51 51 51Zm-127.95 76.95L591-642l51 51-25.95-25.05Z"/></svg>'
+        newModif.appendChild(newModifText)
+        newModif.appendChild(newModifSvg)
         
         let newSupp = document.createElement("button")
         newSupp.innerText = "Supprimer"
         
         let newInput = document.createElement("input")
         newInput.placeholder = " Modification"
+        newInput.style = "border-radius: 5px; border: none;"
         newInput.style.display = "none"
 
         divBtnTache.appendChild(newInput)
@@ -82,9 +88,11 @@ btnAjouter.addEventListener("click", ()=>{
             let listeVide = document.getElementById("listeVide")
             if (liste.children.length === 0) {
                 listeVide.style.display = "flex"
+                liste.style.display = "none"
             }
             else if (liste.children.length >= 1) {
                 listeVide.style.display = "none"
+                liste.style.display = "flex"
             }
         })
         newModif.addEventListener("click", ()=> {
@@ -139,13 +147,15 @@ btnAjouter.addEventListener("click", ()=>{
                 newTache.style.display = "none";
             }
         })
-            let listeVide = document.getElementById("listeVide")
-    if (liste.children.length === 0) {
-        listeVide.style.display = "flex"
-    }
-    else if (liste.children.length >= 1) {
-        listeVide.style.display = "none"
-    }
+        let listeVide = document.getElementById("listeVide")
+        if (liste.children.length === 0) {
+            listeVide.style.display = "flex"
+            liste.style.display = "none"
+        }
+        else if (liste.children.length >= 1) {
+            listeVide.style.display = "none"
+            liste.style.display = "flex"
+        }
 })
 inputAjouter.addEventListener("keypress", (e)=>{
     if (e.key === "Enter"){
@@ -153,12 +163,14 @@ inputAjouter.addEventListener("keypress", (e)=>{
         btnAjouter.click()
     }
     let listeVide = document.getElementById("listeVide")
-    if (liste.children.length === 0) {
-        listeVide.style.display = "flex"
-    }
-    else if (liste.children.length >= 1) {
-        listeVide.style.display = "none"
-    }
+        if (liste.children.length === 0) {
+            listeVide.style.display = "flex"
+            liste.style.display = "none"
+        }
+        else if (liste.children.length >= 1) {
+            listeVide.style.display = "none"
+            liste.style.display = "flex"
+        }
 })
 
 btnTout.addEventListener("click", ()=>{
@@ -166,6 +178,9 @@ btnTout.addEventListener("click", ()=>{
     Array.from(liste.children).forEach(li => {
         li.style.display = "flex"
     })
+    btnTout.style = "background-color: #3d3d3d; color: white"
+    btnNonTerminer.style = "background-color: rgba(255, 255, 255, 0.75); color: black"
+    btnTerminer.style = "background-color: rgba(255, 255, 255, 0.75); color: black"
 })
 btnNonTerminer.addEventListener("click", ()=>{
     modeAffichage = "NonTerminer"
@@ -177,6 +192,9 @@ btnNonTerminer.addEventListener("click", ()=>{
             li.style.display = "none";
         }
     });
+    btnNonTerminer.style = "background-color: #3d3d3d; color: white"
+    btnTerminer.style = "background-color: rgba(255, 255, 255, 0.75); color: black"
+    btnTout.style = "background-color: rgba(255, 255, 255, 0.75); color: black"
 })
 btnTerminer.addEventListener("click", ()=>{
     modeAffichage = "Terminer"
@@ -188,6 +206,8 @@ btnTerminer.addEventListener("click", ()=>{
             li.style.display = "none"
         }
     })
-
+    btnTerminer.style = "background-color: #3d3d3d; color: white"
+    btnNonTerminer.style = "background-color: rgba(255, 255, 255, 0.75); color: black"
+    btnTout.style = "background-color: rgba(255, 255, 255, 0.75); color: black"
 })
 
